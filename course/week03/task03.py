fa97683566b49bcd4c21c40506b5000aad477b59
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-23 15:28:50 +0100

    Update week03/task03.py

--- course/week03/task03.py
+++ course/week03/task03.py
@@ -10,5 +10,15 @@ Vystup: "A"
 """
 
 def solve(skore):
-    """Implementuj podľa zadania v hlavičke."""
-    raise NotImplementedError
+    if (skore>=50) and (skore<60):
+        return "E"
+    if (skore>=60) and (skore<70):
+        return "D"
+    if (skore>=70) and (skore<80):
+        return "C"
+    if (skore>=80) and (skore<90):
+        return "B"
+    if (skore>=90):
+        return "A"
+    else:
+        return "FX" 
